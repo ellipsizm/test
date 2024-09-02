8aacf59f19010a3916145ff26bb1786974d54339
ellipsizm <sioloni143@gmail.com>
2024-09-02 19:51:20 +0300

    second commit

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+
+import com.example.demo.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+    private User user;
+}
